1ef7286e872c817fb45bc4480f4058b3e9a89865
abalcs <abalcom23@gmail.com>
2021-04-07 17:41:38 -0400

    js updates

--- src/script.js
+++ src/script.js
@@ -1,6 +1,32 @@
 var weekDay = moment();
 $("#currentDay").text(weekDay.format('dddd, MMMM Do YYYY HH:mm A'));
 
+let nine = document.getElementById('#09');
+let ten = document.getElementById('#10');
+let eleven = document.getElementById('#11');
+let twelve = document.getElementById('#12');
+let one = document.getElementById('#13');
+let two = document.getElementById('#14');
+let three = document.getElementById('#15');
+let four = document.getElementById('#16');
+let five = document.getElementById('#17');
+let six = document.getElementById('#18');
+
+let row = document.querySelectorAll('.time-row');
+
+let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+const now = moment().hour();
+
+for (const hour of hours) {
+
+  if (now < hour) {
+    
+  } else if(now === hour) {
+
+  } else {
+
+  }
+}
 
 
 /*
