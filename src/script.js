702c90cc78c90421a78286d3b13b9797db4722dd
abalcs <abalcom23@gmail.com>
2021-04-08 11:30:40 -0400

    saving updates to js comments & style updates

--- src/script.js
+++ src/script.js
@@ -2,11 +2,12 @@ var weekDay = moment();
 $("#currentDay").text(weekDay.format('dddd, MMMM Do YYYY HH:mm A'));
 
 let hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-const now = moment().hour()-7;
+const now = moment().hour();
 
 //Cycling through the array of hours
 for (const hour of hours) {
-  //Setting new variable equal to the hour within the array and converting it to a string
+  // row variable represents the html element which has an ID matching the value 'hour' (converted to a string)
+
   let row = document.getElementById(hour.toString());
   //Retrieving the value of given hour from local storage
   const value = localStorage.getItem(hour);
@@ -25,7 +26,8 @@ for (const hour of hours) {
 
 //Getting each save button and telling them to perform this function on each click
 $(".saveBtn").on('click', function(){
-  //Assigning an ID attribute to the parent div of the save button and assigning that to a new variable called ID
+  // Assigning an ID attribute of the parent div of the save button to a new variable called ID
+
   const id = $(this).parent().attr('id');
   //Getting the value of the text area by targeting the parent div and finding the element of text area and assigning its' value to a variable called value.
   const value = $(this).parent().find('textarea').val()
